098a401b12099babfde1a74fb23cf003e3c3324a
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-01-13 14:10:18 -0500

    testing backend code to contact form

--- api/routes/ContactForm.ts
+++ api/routes/ContactForm.ts
@@ -1,30 +1,42 @@
-import express, { Request, Response } from 'express'
-import ContactForm from '../models/ContactFormSchema.js'
+import express from 'express'
 
 const router = express.Router()
 
-interface ContactFormRequest extends Request {
-  body: {
-    name: string
-    email: string
-    subject?: string
-    message: string
-  }
-}
-router.post('/contact', async (req: ContactFormRequest, res: Response) => {
-  console.log('Incoming request:', req.body)
-  try {
-    const { name, email, subject, message } = req.body
-
-    const newContact = new ContactForm({ name, email, subject, message })
-    await newContact.save()
-
-    console.log('Contact form saved successfully:', newContact)
-    res.status(201).json({ message: 'Contact form submitted successfully' })
-  } catch (error) {
-    console.error('Error saving contact form:', error)
-    res.status(500).json({ error: 'Failed to submit the contact form' })
-  }
+// POST /contact route
+router.post('/contact', (req, res) => {
+  console.log('Contact form submitted successfully!')
+  res.status(200).send({ message: 'Contact form submitted successfully!' })
 })
 
 export default router
+
+// import express, { Request, Response } from 'express'
+// import ContactForm from '../models/ContactFormSchema.js'
+
+// const router = express.Router()
+
+// interface ContactFormRequest extends Request {
+//   body: {
+//     name: string
+//     email: string
+//     subject?: string
+//     message: string
+//   }
+// }
+// router.post('/contact', async (req: ContactFormRequest, res: Response) => {
+//   console.log('Incoming request:', req.body)
+//   try {
+//     const { name, email, subject, message } = req.body
+
+//     const newContact = new ContactForm({ name, email, subject, message })
+//     await newContact.save()
+
+//     console.log('Contact form saved successfully:', newContact)
+//     res.status(201).json({ message: 'Contact form submitted successfully' })
+//   } catch (error) {
+//     console.error('Error saving contact form:', error)
+//     res.status(500).json({ error: 'Failed to submit the contact form' })
+//   }
+// })
+
+// export default router
