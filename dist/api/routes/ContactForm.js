4a14abf139322f6640d51f200fb94dcab2bf90e3
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-01-13 17:44:13 -0500

    added new build

--- dist/api/routes/ContactForm.js
+++ dist/api/routes/ContactForm.js
@@ -1,33 +1,36 @@
-import express from 'express';
-const router = express.Router();
-// POST /contact route
-router.post('/contact', (req, res) => {
-    console.log('Contact form submitted successfully!');
-    res.status(200).send({ message: 'Contact form submitted successfully!' });
-});
-export default router;
-// import express, { Request, Response } from 'express'
-// import ContactForm from '../models/ContactFormSchema.js'
+/* eslint-disable import/first */
+// import express from 'express'
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // const router = express.Router()
-// interface ContactFormRequest extends Request {
-//   body: {
-//     name: string
-//     email: string
-//     subject?: string
-//     message: string
-//   }
-// }
-// router.post('/contact', async (req: ContactFormRequest, res: Response) => {
-//   console.log('Incoming request:', req.body)
-//   try {
-//     const { name, email, subject, message } = req.body
-//     const newContact = new ContactForm({ name, email, subject, message })
-//     await newContact.save()
-//     console.log('Contact form saved successfully:', newContact)
-//     res.status(201).json({ message: 'Contact form submitted successfully' })
-//   } catch (error) {
-//     console.error('Error saving contact form:', error)
-//     res.status(500).json({ error: 'Failed to submit the contact form' })
-//   }
+// // POST /contact route
+// router.post('/contact', (req, res) => {
+//   console.log('Contact form submitted successfully!')
+//   res.status(200).send({ message: 'Contact form submitted successfully!' })
 // })
 // export default router
+import express from 'express';
+import ContactForm from '../models/ContactFormSchema.js';
+const router = express.Router();
+router.post('/contact', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('Incoming request:', req.body);
+    try {
+        const { name, email, subject, message } = req.body;
+        const newContact = new ContactForm({ name, email, subject, message });
+        yield newContact.save();
+        console.log('Contact form saved successfully:', newContact);
+        res.status(201).json({ message: 'Contact form submitted successfully' });
+    }
+    catch (error) {
+        console.error('Error saving contact form:', error);
+        res.status(500).json({ error: 'Failed to submit the contact form' });
+    }
+}));
+export default router;
