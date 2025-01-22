f2eab471394bb26ada10a43b2cd203018e1c1fa3
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-01-21 21:12:56 -0500

    changed vercel json file added rewrites and sentiment page added original code

--- src/pages/SentimentPage.tsx
+++ src/pages/SentimentPage.tsx
@@ -1,30 +1,30 @@
 import React, { useState } from 'react'
-// const apiUrl = process.env.REACT_APP_PY_URL || 'http://127.0.0.1:5002'
+const apiUrl = process.env.REACT_APP_PY_URL || 'http://127.0.0.1:5002'
 
 export default function SentimentPage() {
   const [inputText, setInputText] = useState<string>('')
-  // const [result, setResult] = useState<string | null>(null)
-  // const [error, setError] = useState<string | null>(null)
-  // const analyzeSentiment = async () => {
-  //   setError(null)
-  //   setResult(null)
-  //   try {
-  //     const response = await fetch('http://kimudev.onrender.com/predict', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({ text: inputText }),
-  //     })
-  //     if (!response.ok) {
-  //       throw new Error('Failed to fetch sentiment analysis.')
-  //     }
-  //     const data = await response.json()
-  //     setResult(data.sentiment)
-  //   } catch (err: any) {
-  //     setError(err.message)
-  //   }
-  // }
+  const [result, setResult] = useState<string | null>(null)
+  const [error, setError] = useState<string | null>(null)
+  const analyzeSentiment = async () => {
+    setError(null)
+    setResult(null)
+    try {
+      const response = await fetch(`${apiUrl}/predict`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: inputText }),
+      })
+      if (!response.ok) {
+        throw new Error('Failed to fetch sentiment analysis.')
+      }
+      const data = await response.json()
+      setResult(data.sentiment)
+    } catch (err: any) {
+      setError(err.message)
+    }
+  }
   return (
     <div className="bg-gray-900 text-white min-h-screen p-8">
       <h1 className="text-4xl font-bold mb-4">Sentiment Analysis</h1>
@@ -39,7 +39,7 @@ export default function SentimentPage() {
         onChange={(e) => setInputText(e.target.value)}
         className="w-full h-32 p-2 mb-4 text-black"
       />
-      {/* <button
+      <button
         onClick={analyzeSentiment}
         className="bg-blue-500 text-white px-4 py-2 rounded"
       >
@@ -55,7 +55,7 @@ export default function SentimentPage() {
         <div className="mt-4 text-red-500">
           <strong>Error:</strong> {error}
         </div>
-      )} */}
+      )}
     </div>
   )
 }
