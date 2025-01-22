301608e7af5703ca1850ef7ec88ebe4a438b2579
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-01-21 21:16:03 -0500

    changed vercel json file added rewrites and sentiment page added original code

--- src/pages/SentimentPage.tsx
+++ src/pages/SentimentPage.tsx
@@ -39,6 +39,7 @@ export default function SentimentPage() {
         onChange={(e) => setInputText(e.target.value)}
         className="w-full h-32 p-2 mb-4 text-black"
       />
+
       <button
         onClick={analyzeSentiment}
         className="bg-blue-500 text-white px-4 py-2 rounded"
