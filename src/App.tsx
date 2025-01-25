6798b853f013f313f6738864c76e0152c10d3130
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-01-24 20:03:05 -0500

    Cleaned up app file and reverted package.json and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,21 @@ import IntroductionSection from './components/IntroductionSection'
 import ExpertiseSection from './components/ExpertiseSection'
 import WorkSection from './components/WorkSection'
 import ContactSection from './components/ContactSection'
+import ScrollToTop from './components/ScrollToTop'
+import CustomCursor from './components/CustomCursor'
 import ScrollController from './components/ScrollController'
 import SentimentPage from './pages/SentimentPage'
 import MachineLearning from './pages/MachineLearning'
-import ScrollToTop from './components/ScrollToTop'
-import CustomCursor from './components/CustomCursor'
 import EmploymentOne from './components/Employment_one'
 import EmploymentTwo from './components/Employment_two'
 import EmploymentThree from './components/Employment_three'
-import './App.css'
 import ProjectList from './pages/ProjectList'
+import './App.css'
+
 const App: React.FC = () => {
   return (
     <Router>
-      <ScrollToTop />
+      <ScrollToTop /> {/* Now inside the Router */}
       <CustomCursor />
       <ScrollController />
       <Navigation />
@@ -36,7 +37,6 @@ const App: React.FC = () => {
 const MainPage: React.FC = () => {
   return (
     <div className="bg-black text-white min-h-screen">
-      <Navigation />
       <section id="header" className="h-screen">
         <Header />
       </section>
@@ -46,13 +46,13 @@ const MainPage: React.FC = () => {
       <section id="expert" className="h-screen">
         <ExpertiseSection />
       </section>
-      <section id="profile" className="h-screen">
+      <section id="frontend-experience" className="h-screen">
         <EmploymentOne />
       </section>
-      <section id="profile" className="h-screen">
+      <section id="backend-experience" className="h-screen">
         <EmploymentTwo />
       </section>
-      <section id="profile" className="h-screen">
+      <section id="project-experience" className="h-screen">
         <EmploymentThree />
       </section>
       <section id="work" className="h-screen">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all relevant file types in your `src` directory
+    "./src/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
-    extend: {}, // Customize the default theme here if needed
+    extend: {}, 
   },
   plugins: [],
 };
