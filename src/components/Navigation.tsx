6ec6e312b7b9b2c168096698adf7115f07b85bfa
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-02-17 16:24:14 -0500

    fix(navigation): update onClick handler to route non-home pages to home then scroll

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-scroll'
+import { useLocation, useNavigate } from 'react-router-dom'
 import {
   FaHome,
   FaUser,
@@ -7,8 +8,26 @@ import {
   FaCode,
   FaEnvelope,
 } from 'react-icons/fa'
+import { scroller } from 'react-scroll'
 
 const Navigation: React.FC = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const handleClick = (target: string) => {
+    if (location.pathname !== '/') {
+      navigate('/')
+      if (target !== 'introduction-section') {
+        setTimeout(() => {
+          scroller.scrollTo(target, {
+            smooth: true,
+            duration: 500,
+            offset: -70,
+          })
+        }, 100)
+      }
+    }
+  }
   return (
     <nav className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-gray-900 p-3 rounded-full flex space-x-4 shadow-lg z-50">
       <Link
@@ -20,6 +39,7 @@ const Navigation: React.FC = () => {
         spy={true}
         offset={-70}
         aria-label="Home"
+        onClick={() => handleClick('introduction-section')}
       >
         <FaHome size={24} />
       </Link>
@@ -33,6 +53,7 @@ const Navigation: React.FC = () => {
         spy={true}
         offset={-70}
         aria-label="Expertise"
+        onClick={() => handleClick('expertise-section')}
       >
         <FaUser size={24} />
       </Link>
@@ -45,6 +66,7 @@ const Navigation: React.FC = () => {
         spy={true}
         offset={-70}
         aria-label="Work Experience"
+        onClick={() => handleClick('work-experience')}
       >
         <FaProjectDiagram size={24} />
       </Link>
@@ -58,6 +80,7 @@ const Navigation: React.FC = () => {
         spy={true}
         offset={-70}
         aria-label="Projects"
+        onClick={() => handleClick('project-section')}
       >
         <FaCode size={24} />
       </Link>
@@ -71,6 +94,7 @@ const Navigation: React.FC = () => {
         spy={true}
         offset={-70}
         aria-label="Contact"
+        onClick={() => handleClick('contact-me')}
       >
         <FaEnvelope size={24} />
       </Link>
